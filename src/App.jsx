1161a5bb298fe6eaaf8512c1f0f7a2588a5619c5
Vannak <heyvannak3@gmail.com>
2025-06-17 23:54:10 +0700

    ShopCard2

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import { Outlet } from "react-router-dom"
 import Header from "./components/Header"
 import Hero from "./components/Hero"
 import ShopCategories from "./components/ShopCategories"
-import Footer from "./Components/footer"
+import Footer from "./components/footer"
 
 
 
